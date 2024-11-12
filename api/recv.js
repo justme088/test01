f35754af1f05f05d91ec17ca50d48b53154966cc
justme088 <39110970+justme088@users.noreply.github.com>
2024-11-12 13:59:15 -0500

    Update recv.js

--- api/recv.js
+++ api/recv.js
@@ -1,17 +1,21 @@
-// api/recv.js
+// api/addData.js
 
 let datos = global.datos || [];
 global.datos = datos;
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
-    const { s, T, P, H } = req.query;
+    try {
+      const { s, T, P, H } = req.body;
 
-    if (s && T && P && H) {
-      datos.push({ s, T, P, H });
-      res.status(200).json({ message: 'Datos agregados con éxito' });
-    } else {
-      res.status(400).json({ message: 'Faltan campos en el JSON' });
+      if (s && T && P && H) {
+        datos.push({ s, T, P, H });
+        res.status(200).json({ message: 'Datos agregados con éxito' });
+      } else {
+        res.status(400).json({ message: 'Faltan campos en el JSON' });
+      }
+    } catch (error) {
+      res.status(400).json({ message: 'Error en el formato del JSON' });
     }
   } else {
     res.status(405).json({ message: 'Método no permitido' });
