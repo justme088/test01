a28ff564bcf6644f1792795bc4b2ec7b043506de
justme088 <39110970+justme088@users.noreply.github.com>
2024-11-12 13:32:14 -0500

    Update clearData.js

--- api/clearData.js
+++ api/clearData.js
@@ -1,1 +1,14 @@
 
+// api/clearData.js
+
+let datos = global.datos || [];
+global.datos = datos;
+
+export default async function handler(req, res) {
+  if (req.method === 'GET') {
+    datos.length = 0; // Limpia los datos en el array
+    res.status(200).json({ message: 'Datos limpiados con éxito' });
+  } else {
+    res.status(405).json({ message: 'Método no permitido' });
+  }
+}
